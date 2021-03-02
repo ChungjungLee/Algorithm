44b577fd7eb43cb3da6633f1c2e531077ebabd8b
ChungjungLee <cj.chungjung.lee@gmail.com>
2021-03-02 22:53:29 +0900

    Implementation / Strange Counter

---
+++ src/ps/hackerrank/StrangeCounter.java
@@ -0,0 +1,46 @@
+/*
+    https://www.hackerrank.com/challenges/strange-code/problem
+ */
+package ps.hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the strangeCounter function below.
+    static long strangeCounter(long t) {
+        long groupNum = 3;
+        long time = 0;
+
+        while (time < t) {
+            time += groupNum;
+            groupNum *= 2;
+        }
+
+        return time - t + 1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        long t = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = strangeCounter(t);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
