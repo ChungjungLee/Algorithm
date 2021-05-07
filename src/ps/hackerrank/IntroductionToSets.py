553b2833ed71ce337ffb51df6f9668053c32cf0e
Chungjung Lee <cj.chungjung.lee@gmail.com>
2021-05-07 23:30:05 +0900

    Sets / Introduction to Sets

---
+++ src/ps/hackerrank/IntroductionToSets.py
@@ -0,0 +1,13 @@
+'''
+    https://www.hackerrank.com/challenges/py-introduction-to-sets/problem
+'''
+def average(array):
+    # your code goes here
+    return round(sum(set(array)) / len(set(array)), 3)
+    
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
