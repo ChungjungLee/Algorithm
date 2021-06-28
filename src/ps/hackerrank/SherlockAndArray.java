8c0f1ea3376989539ce7c585f081690fdde6087d
Chungjung Lee <cj.chungjung.lee@gmail.com>
2021-06-28 23:18:47 +0900

    Search / Sherlock and Array

---
+++ src/ps/hackerrank/SherlockAndArray.java
@@ -0,0 +1,71 @@
+/*
+    https://www.hackerrank.com/challenges/sherlock-and-array/problem
+ */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'balancedSums' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static String balancedSums(List<Integer> arr) {
+    // Write your code here
+        int[] sum = new int[arr.size() + 1];
+        
+        for (int i = 1; i < sum.length; i++) {
+            sum[i] += sum[i - 1] + arr.get(i - 1);
+        }
+        
+        for (int i = 1; i < sum.length; i++) {
+            if (sum[i - 1] == sum[sum.length - 1] - sum[i]) {
+                return "YES";
+            }
+        }
+        
+        return "NO";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, T).forEach(TItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                String result = Result.balancedSums(arr);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
